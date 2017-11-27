d776a0f5dd5f87ba85c0eab1d3b5831536cd01de
salvastorie <salvastorie@salvastorie-Meware>
2017-11-27 17:14:16 +0100

    added log

--- src/main/java/com/sprhib/controller/LinkController.java
+++ src/main/java/com/sprhib/controller/LinkController.java
@@ -1,19 +1,44 @@
 package com.sprhib.controller;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.sprhib.init.WebAppConfig;
+
 @Controller
 public class LinkController {
 	
+	@Autowired
+	private WebAppConfig wac;
+	
 	@RequestMapping(value="/")
 	public ModelAndView mainPage() {
-		return new ModelAndView("home");
+		return initialPage();
 	}
 	
 	@RequestMapping(value="/index")
 	public ModelAndView indexPage() {
-		return new ModelAndView("home");
+		return initialPage();
+	}
+	
+	private ModelAndView initialPage(){
+		String s;
+		try {
+			DriverManagerDataSource dmds = (DriverManagerDataSource)wac.dataSource();
+			s = dmds.getUrl();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			s = e.getMessage();
+		}
+		ModelAndView mav = new ModelAndView("home");
+		mav.addObject("message", s);
+		return mav;
 	}
 }
